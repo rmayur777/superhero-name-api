af791847f9a4fe9af9d968453f5501e9a3df12cf
Mayur Rathod <rmayur777@gmail.com>
2023-07-01 17:27:18 +0530

    first working superhero api

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const superheroes = {
+'superman' : {
+    'age': 32,
+    'birthName':'Kal-el',
+    'birthLocation':'Krypton'
+},
+
+'batman' : {
+    'age': 34,
+    'birthName':'Bruce Wayne',
+    'birthLocation':'Gotham'
+},
+
+'unknown' : {
+    'age': 0,
+    'birthName':'unknown',
+    'birthLocation':'unknown'
+}
+
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.listen(PORT,() =>{
+    console.log(`Server is running on PORT ${PORT}`)
+})
+
+app.get('/api/:name', (request, response)=>{
+    const superheroName = request.params.name.toLowerCase()
+    if (superheroes[superheroName]){
+        response.json(superheroes[superheroName])
+    }else{
+        response.json(superheroes['unknown'])
+    }
+    
+})
